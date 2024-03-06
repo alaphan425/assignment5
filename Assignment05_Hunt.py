80471ed6d5f2ff93f4a3148d53f033a2ae4e46f2
alaphan425 <157992184+alaphan425@users.noreply.github.com>
2024-03-05 19:11:47 -0500

    Add files via upload
    Finished class and most of main()

--- Assignment05_Hunt.py
+++ Assignment05_Hunt.py
@@ -4,7 +4,7 @@ EECE 2140
 Professor Fatema Nafa
 March 5, 2024
 """
-class BasicMathOperations:
+class BasicMathOperations:   
     def greetUser(self, forename, surname):
         print("Hello, "+forename+" "+surname+".")
         
@@ -42,6 +42,16 @@ class BasicMathOperations:
             return result
         else:
             print("Error: Factorial only defined for positive integers.")
+            
+    def countNumbers(self, num1, num2):
+        if num1>num2:
+            for i in range (num1, num2+1):
+                print(i)
+        elif num2>num1:
+           for i in range (num1, num2-1, -1):
+               print(i)
+        else:
+            print("Error: Unable to count when numbers are equal.")
     
     def findHypotenuse(self, side1, side2):
         result = (self.calculate_square(side1)+self.calculate_square(side2))**(1/2)
@@ -61,5 +71,49 @@ class BasicMathOperations:
         return result
     
     def findType(self, argument):
+        result = type(argument).__name__
+        return result
+
+def main():
+    end = False
+    while end == False:
+        print("Welcome to the python calculator tool.")
+        print("                                      ")
+        print("Please enter one of the number next to the functions below to select it:")
+        print("1 - Greet User")
+        print("2 - Add Numbers")
+        print("3 - Perform Basic Arithmetic Operations")
+        print("4 - Calculate Square of Number")
+        print("5 - Calculate Factorial of Integer")
+        print("6 - Count Numbers")
+        print("7 - Find Hypotenuse of Right Triangle")
+        print("8 - Find Area of Rectangle")
+        print("9 - Compute Power of a Number")
+        print("10 - Return Datatype of Input")
+        option = input()
         
-        
+        if option == 1:
+            forename = input("Please input your name")
+            surname = input("Please enter your surname")
+            BasicMathOperations.greetUser(forename,surname)
+        elif option == 2:
+            
+        elif option == 3:
+            
+        elif option == 4:
+            
+        elif option == 5:
+            
+        elif option == 6:
+            
+        elif option == 7:
+            
+        elif option == 8:
+            
+        elif option == 9:
+            
+        elif option == 10:
+            
+        elif option == 11:
+            
+main()
